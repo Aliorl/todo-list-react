1a1dbb7fd9b741d300baf1d3c0d3990983e95b8e
Alicja Orlik <88146004+Aliorl@users.noreply.github.com>
2023-03-12 14:36:49 +0100

    toggle task done interaction added

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header title="To Do List App" />
@@ -37,7 +47,8 @@ function App() {
           <Tasks
             tasks={tasks}
             hideDone={hideDone}
-            removeTask={removeTask} />
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone} />
         }
         extraHeaderContent={
           <Buttons
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,10 +1,17 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
-            <li key={task.id} className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`}>
-                <button className="tasks__button tasks__button--toggleDone">
+            <li key={task.id} className={
+                `tasks__item${task.done && hideDone
+                    ? " tasks__item--hidden"
+                    : ""}`
+            }>
+                <button
+                    className="tasks__button tasks__button--toggleDone"
+                    onClick={() => toggleTaskDone(task.id)}
+                >
                     {task.done ? "✓" : ""}
                 </button>
                 <span className={`tasks__item${task.done ? "tasks__item tasks__item--done" : "tasks__item"}`}>
