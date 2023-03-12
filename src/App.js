78e62afcbb82ab7e6576db9a8069f216730fb60f
Alicja Orlik <88146004+Aliorl@users.noreply.github.com>
2023-03-12 13:59:22 +0100

    toggleHideDone interaction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './toDoList.svg';
+import { useState } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -11,12 +11,15 @@ const tasks = [
   { id: 2, content: "go for a walk", done: true },
 ];
 
-const hideDone = false;
-
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
   return (
     <Container>
-
       <Header title="To Do List App" />
 
       <Section
@@ -27,9 +30,13 @@ function App() {
       <Section
         title="To Do List"
         body={<Tasks tasks={tasks} hideDone={hideDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone} />
+        }
       />
-
     </Container >
   );
 }
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Tasks = ({ tasks, hideDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
-            <li key={task.id} className={`tasks__item${task.done && hideDone ? "tasks__item--hidden" : ""}`}>
+            <li key={task.id} className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`}>
                 <button className="tasks__button tasks__button--toggleDone">
                     {task.done ? "✓" : ""}
                 </button>
